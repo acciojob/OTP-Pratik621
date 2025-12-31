68b34cd61da752724272f720904d46ca7948207e
Pratik Mane <manepratik3727@gmail.com>
2025-12-31 22:19:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const codes=document.querySelectorAll(".code")
+codes[0].focus()
+
+codes.forEach((code,index)=>{
+	code.addEventListener("keydown",(e)=>{
+		if(e.key>="0" && e.key<="9"){
+			code.value="";
+			setTimeout(()=>{
+				if(codes[index+1]){
+					codes[index+1].focus();
+				}
+			},10)
+		}else if(e.key==="Backspace"){
+			code.value="";
+			if(codes[index-1]){
+				codes[index-1].focus()
+			}
+		}
+	})
+})
